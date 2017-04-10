cd01deb3eb56429be7479fa4eaa93976d3b1ecd3
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2017-04-10 13:23:26 +0200

    IoT Hub Nuget packages

--- DotnetBackend/Program.cs
+++ DotnetBackend/Program.cs
@@ -11,6 +11,10 @@ namespace DotnetBackend
         static void Main(string[] args)
         {
             Console.Title = "Backend";
+            var key = Environment.GetEnvironmentVariable("AZURE_IOT_HUB_OWNER_KEY");
+
+            Console.WriteLine($"{key}");
+
         }
     }
 }
